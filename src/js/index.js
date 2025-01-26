0158a1110ee1f52f5ebc353ff130ac0328c20fe3
1tsc0sta <mateus.costa74p@gmail.com>
2025-01-25 23:36:14 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+console.log(document.getElementById("btn-avancar"));
+
+//identificar clique
+
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao")
+let cartaoAtual = 0;
+
+btnAvancar.addEventListener("click", function () {
+    if (cartaoAtual === cartoes.length - 1) return;
+   
+    esconderCartaoSelecionado();
+   
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);                      //ponto diz o que tem ali
+});
+
+
+btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
+    
+    esconderCartaoSelecionado();
+    
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual) {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
