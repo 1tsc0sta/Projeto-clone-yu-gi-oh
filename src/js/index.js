10c95431415aea990a07b5a3cc6c8c00f696da0a
1tsc0sta <mateus.costa74p@gmail.com>
2025-01-31 20:01:15 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,27 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao")
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVirada = cartao.querySelector(".carta-virada")
+        //virar cartao
+        cartao.classList.toggle("virar");
+        //mostrar fundo carta
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+       
+    });
+
+});
+
+
+
 btnAvancar.addEventListener("click", function () {
     if (cartaoAtual === cartoes.length - 1) return;
-   
+
     esconderCartaoSelecionado();
-   
+
     cartaoAtual++;
     mostrarCartao(cartaoAtual);                      //ponto diz o que tem ali
 });
@@ -19,9 +35,9 @@ btnAvancar.addEventListener("click", function () {
 
 btnVoltar.addEventListener("click", function () {
     if (cartaoAtual === 0) return;
-    
+
     esconderCartaoSelecionado();
-    
+
     cartaoAtual--;
     mostrarCartao(cartaoAtual);
 });
